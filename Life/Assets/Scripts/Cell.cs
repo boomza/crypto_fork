bdae8e7cfcc0ee715ce32e5e7ad4de4879aa6c4d
co6aka <moryak@gmail.com>
2024-09-23 14:16:06 -0400

    Game of Life baseline demo

---
+++ Life/Assets/Scripts/Cell.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class Cell : MonoBehaviour
+{
+    public Sprite liveImg, deadImg;
+    private SpriteRenderer sr;
+    public bool aliveNow = false, aliveNextGen = false;
+
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    // migrate next state to current
+    public void Evolve()
+    {
+        aliveNow = aliveNextGen;
+        aliveNextGen = false;
+        Repaint();
+    }
+
+    private void OnMouseUpAsButton()
+    {
+        aliveNow = !aliveNow;
+        Repaint();
+    }
+
+    void Repaint()
+    {
+        sr.sprite = aliveNow ? liveImg : deadImg;
+    }
+
+    public override string ToString()
+    {
+        return "" + aliveNow;
+    }
+}
---
+++ Life/Assets/Scripts/Generator.cs
@@ -0,0 +1,55 @@
+﻿public class Generator {
+
+    public static void Evolve(Cell[,] cells)
+    {
+        int rows = cells.GetLength(0);
+        int cols = cells.GetLength(1);
+
+        // determine next state from the current
+        for (int x = 1; x < rows - 1; x++)      // leave out the borders
+            for (int y = 1; y < cols - 1; y++)
+                ApplyRule3x3(cells, y, x);      // trt
+
+        // apply next state to the current
+        for (int x = 1; x < rows - 1; x++)
+            for (int y = 1; y < cols - 1; y++)
+                cells[y, x].Evolve();
+    }
+
+    /**********************************************************************************
+     * Rules:
+     Every cell interacts with its 8 nearest neighbors
+     Any live cell with two or three neighbors survives   
+     Any dead cell with three neigbors awakens
+     **********************************************************************************/
+    static void ApplyRule3x3(Cell[,] neighbors, int x, int y)
+    {
+        int count = 0;
+        Cell me = neighbors[x, y];
+
+        // count nearest 8 neighbors, and depending on the result
+        // set this cell's state to either alive or dead
+        for (int row = x - 1; row <= x + 1; row++)
+        {
+            for(int col = y-1; col <= y+1; col++)
+            {
+                if (row == x && col == y)    
+                    continue;       // skip myself
+                if (neighbors[row, col].aliveNow)
+                    count++;
+            }
+        }
+
+        if(me.aliveNow)
+        {
+            if (count < 2 || count > 3)
+                me.aliveNextGen = false;
+            else
+                me.aliveNextGen = true;     // survives
+        }
+        else if (count == 3)
+            me.aliveNextGen = true;
+
+    }
+
+}
